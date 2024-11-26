ac67e13ee1df9b9db97084b7e1e43c1cfdcc714d
DiogoMJs <00001118030242sp@al.educacao.sp.gov.br>
2024-11-26 00:02:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Tocantins?',
+    'A capital de Tocantins é Palmas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em inglês é Love (ləv)'
+)
